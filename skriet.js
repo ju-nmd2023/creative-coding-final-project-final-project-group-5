760ec84e46dc37225a38ec6d524098d62d9f773c
lofquista <loal23gk@student.ju.se>
2025-10-10 19:00:18 +0200

    Face of skriet

--- skriet.js
+++ skriet.js
@@ -10,23 +10,49 @@ const skrietHeight = 300;
 
 function faceSkriet() {
   push();
-  translate(width / 2, height - skrietHeight - 50);
-scale(2);
-translate(-width / 2, -(height - skrietHeight - 60));
-  
-   beginShape();
+ 
+  stroke(204, 152, 105);
+  strokeWeight(0.5);
 fill(204, 142, 105);
+
+// Neck
+beginShape();
+// Left side
+vertex(width / 2 - 30, height - skrietHeight - 40); // Top left
+bezierVertex(
+  width / 2, height - skrietHeight - 20, // Curve outward slightly
+  width / 2 - 30, height - skrietHeight,       // Curve back inward
+  width / 2 - 10, height - skrietHeight + 5   // Bottom left (rounded)
+);
+
+// Bottom curve
+bezierVertex(
+  width / 2, height - skrietHeight + 10,
+  width / 2, height - skrietHeight + 10,
+  width / 2 + 10, height - skrietHeight + 5   // Bottom right (rounded)
+);
+
+// Right side
+bezierVertex(
+  width / 2 + 30, height - skrietHeight,       // Curve back inward
+  width / 2, height - skrietHeight - 20,  // Curve outward slightly
+  width / 2 + 30, height - skrietHeight - 40   // Top right
+);
+endShape();
+  
+// Face
+   beginShape();
 
 vertex(width / 2, height - skrietHeight - 120);
 
 // Right cheek
 bezierVertex(
   width / 2 + 35, height - skrietHeight - 120,  
-  width / 2 + 70, height - skrietHeight - 70,   
+  width / 2 + 60, height - skrietHeight - 70,   
   width / 2 + 25, height - skrietHeight - 25    
 );
 
-// Chin curve
+// Chin
 bezierVertex(
   width / 2 + 10, height - skrietHeight - 5,    
   width / 2 - 10, height - skrietHeight - 5,    
@@ -35,22 +61,160 @@ bezierVertex(
 
 // Left cheek
 bezierVertex(
-  width / 2 - 70, height - skrietHeight - 70,   
+  width / 2 - 60, height - skrietHeight - 70,   
   width / 2 - 35, height - skrietHeight - 120,  
   width / 2, height - skrietHeight - 120       
 );
 
 endShape(CLOSE);
+
+// Eyes
+fill(204, 162, 105);
+ellipse(width / 2 - 22, height - skrietHeight - 75, 12, 14);
+ellipse(width / 2 + 12, height - skrietHeight - 75, 12, 14);
+
+// Pupils
+fill(0);
+ellipse(width / 2 - 23, height - skrietHeight - 72, 5, 5);
+ellipse(width / 2 + 11, height - skrietHeight - 73, 5, 5);
+
+// Eyebrows
+stroke(30);
+strokeWeight(4.5);
+noFill();
+beginShape();
+vertex(width / 2 - 30, height - skrietHeight - 85);
+bezierVertex(
+  width / 2 - 20, height - skrietHeight - 95,
+  width / 2 - 10, height - skrietHeight - 90,
+  width / 2 - 5, height - skrietHeight - 85
+);
+bezierVertex(
+  width / 2 + 10, height - skrietHeight - 95,
+  width / 2 + 20, height - skrietHeight - 90,
+  width / 2 + 25, height - skrietHeight - 85
+);
+endShape();
+
+push();
+translate(-2, -2);
+// Nose
+stroke(200, 130, 100);
+strokeWeight(2);
+noFill();
+beginShape();
+vertex(width / 2 - 8, height - skrietHeight - 65);
+// Left
+bezierVertex(
+  width / 2 - 5, height - skrietHeight - 55,
+  width / 2 - 8, height - skrietHeight - 58,
+  width / 2 - 8, height - skrietHeight - 52
+);
+endShape();
+
+beginShape();
+vertex(width / 2 + 2, height - skrietHeight - 65);
+// Right
+bezierVertex(
+  width / 2, height - skrietHeight - 55,
+  width / 2 + 2, height - skrietHeight - 58,
+  width / 2 + 4, height - skrietHeight - 52
+);
+endShape();
+
+// Nostrils
+fill(65);
+noStroke();
+ellipse(width / 2 - 6, height - skrietHeight - 52, 3, 4);
+ellipse(width / 2 + 1, height - skrietHeight - 53, 2.5, 3.5);
+pop();
+
+// Mouth
+noFill();
+stroke(40);
+strokeWeight(3);
+ellipse(width / 2 - 4, height - skrietHeight - 33, 12, 22);
+
+pop();
+
+}
+
+function handsSkriet() {    
+  // Hands of Skriet
+push();
+// Left hand and arm
+translate(-48, 10);
+beginShape();
+vertex(width / 2, height - skrietHeight - 100); // Top
+
+stroke(208, 150, 105);
+strokeWeight(14);
+bezierVertex(
+  width / 2 - 10, height - skrietHeight - 50,
+  width / 2, height - skrietHeight - 90,
+  width / 2 + 10, height - skrietHeight - 20  // Middle
+);
+endShape();
+
+beginShape();
+vertex(width / 2 + 10, height - skrietHeight - 20); // Top
+
+stroke(18);
+strokeWeight(20);
+bezierVertex(
+  width / 2 + 10, height - skrietHeight,
+  width / 2, height - skrietHeight,
+  width / 2, height - skrietHeight + 40 // Bottom
+);
+endShape();
+pop();
+
+// Right hand and arm
+push();
+translate(46, 10);
+beginShape();
+vertex(width / 2, height - skrietHeight - 100); // Top
+
+stroke(208, 150, 105);
+strokeWeight(14);
+bezierVertex(
+  width / 2 + 10, height - skrietHeight - 50,
+  width / 2, height - skrietHeight - 90,
+  width / 2 - 10, height - skrietHeight - 20  // Middle
+);
+endShape();
+
+beginShape();
+vertex(width / 2 - 10, height - skrietHeight - 20); // Top
+
+stroke(18);
+strokeWeight(20);
+bezierVertex(
+  width / 2 - 10, height - skrietHeight,
+  width / 2, height - skrietHeight,
+  width / 2, height - skrietHeight + 40 // Bottom point
+);
+endShape();
 pop();
 }
 
 function skriet() {
     //The scream + Frida Kahlo
-    noStroke();
+ 
+    // Body of Skriet
+       noStroke();
     fill(0);
     rect(width / 2 - 125, height - skrietHeight, 250, skrietHeight, 120, 120, 0, 0);
 
+push();
+ translate(width / 2, height - skrietHeight - 90);
+scale(2);
+translate(-width / 2, -(height - skrietHeight - 60));
+
+handsSkriet();
 faceSkriet();
+
+pop();
 
 }
 
